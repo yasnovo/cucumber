0af37d5899bbe458540f75af965274cb8faeab45
yasnovo <yasnovo@gmail.com>
2019-02-07 01:24:32 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:flutter_pdf_renderer/flutter_pdf_renderer.dart';
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('Flutter Demo Home Page'),
+          ),
+        body: PdfRenderer(pdfFile: "assets/sample.pdf", width: 500),
+      ),
+    );
+  }
+}
+
+
